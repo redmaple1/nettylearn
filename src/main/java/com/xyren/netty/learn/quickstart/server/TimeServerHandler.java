dfa769ef9150c1ad97804f4878b26eab02658068
renxiaoya <renxiaoya168@163.com>
2020-02-26 17:53:27 +0800

    netty learn quickstart - server

---
+++ src/main/java/com/xyren/netty/learn/quickstart/server/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package com.xyren.netty.learn.quickstart.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+/**
+ * Netty 时间服务器 - 服务端 时间处理器
+ *
+ * @author : renxiaoya
+ * @date : 2020-02-26
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, StandardCharsets.UTF_8);
+        System.out.println("The time server receive order : " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
